16761f8ff1222fb198834d0286e499e247a786b1
R4hul4ffinity <rahul.rajput@affinityeducation.in>
2025-01-12 01:38:42 +0530

    first push

---
+++ src/utils/network/network.ts
@@ -0,0 +1,4 @@
+export const BASE_URL =
+  "https://acchawala-college-backend-fwt7n.ondigitalocean.app/";
+export const BASE_GQL_URL =
+  "https://acchawala-college-backend-fwt7n.ondigitalocean.app/graphql";
