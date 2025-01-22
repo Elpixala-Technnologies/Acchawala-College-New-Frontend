45694ba93a6a139bf99d23e9d17bf6ba23e628d0
R4hul4ffinity <rahul.rajput@affinityeducation.in>
2025-01-22 10:24:27 +0530

    Pushed a change

--- src/graphql/examQuery/examDetails.ts
+++ src/graphql/examQuery/examDetails.ts
@@ -58,7 +58,7 @@ export const getExamDetails = gql`
               }
             }
           }
-          eligibilityLevel {
+          eligibilityLevels {
             data {
               id
               attributes {
